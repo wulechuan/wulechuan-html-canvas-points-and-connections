cd53145ea12aba1962f751e91ccdd748b3c6a079
吴乐川 <wulechuan@live.com>
2017-06-28 01:15:22 +0800

    wip on scaffold and comments

--- source/class-per-file/0 - commonly-shared-definitions/wulechuan-class-feature-impartation-helper.js
+++ source/class-per-file/0 - commonly-shared-definitions/wulechuan-class-feature-impartation-helper.js
@@ -18,26 +18,26 @@
 	 * 	function My2DVector() {
 	 * 		...
 	 * 	}
-	 * 
+	 *
 	 * 	My2DVector.variantsForWulechuanImpartation = {
 	 * 		position2D: { instanceChiefName: 'position', ... },
 	 * 		force2D:    { instanceChiefName: 'force', ... },
 	 * 		velocity2D: { instanceChiefName: 'velocity', ... }
 	 * 	}
-	 * 
+	 *
 	 * 	function My2DPoint() {
 	 * 		wulechuanImpart(My2DVector).as('position2D').to(this);
 	 * 	}
-	 * 
+	 *
 	 * 	function My2DParticle() {
 	 * 		wulechuanImpart(My2DVector).as('position2D').renamedAs('pos').to(this);
-	 * 
+	 *
 	 * 		wulechuanImpart(My2DVector).as('velocity2D').renamedAs({
 	 * 			speed: 'velocityLength',
 	 * 			speed2: 'squareSpeed'
 	 * 			velocityDirection: 'movingDirection'
 	 * 		}).to(this);
-	 * 
+	 *
 	 * 		wulechuanImpart(My2DVector)
 	 * 			.buildAccordingTo({
 	 * 				x: 3,
@@ -50,7 +50,7 @@
 	 * 				y: 'centerY'
 	 * 			})
 	 * 			.to(this);
-	 * 
+	 *
 	 * 		wulechuanImpart(My2DVector)
 	 * 			.configuredAs('force2D')
 	 * 			.withCustomizedPropertyNames({
@@ -59,11 +59,11 @@
 	 * 			})
 	 * 			.to(this);
 	 * 	}
-	 * 
+	 *
 	 * 	My2DParticle.variantsForWulechuanImpartation = {
 	 * 		default: { instanceChiefName: 'particle2D', ... }
 	 * 	};
-	 * 
+	 *
 	 * 	var myLovelyObjectLiteral = { name: '吴乐川', email: 'wulechuan@live.com' };
 	 *  wulechuanImpart(My2DParticle).to(myLovelyObjectLiteral);
 	 */
@@ -114,6 +114,15 @@
 
 
 
+		/**
+		 * The function, the wrapped versions of which are exposed.
+		 * Wrapping is essentially for providing this function
+		 * in multiple human languages.
+		 *
+		 * Each wrapped version creates an instance of the class "WulechuanImpartationOperator",
+		 * and then calls the "startToImpart" method of the instance
+		 * to complete desired impartation process.
+		 */
 		function impart() {
 			var args = sliceArray.apply(arguments);
 			var usingLanguage = args.unshift();
@@ -129,13 +138,13 @@
 		 * A class, instance of which is the operator
 		 * that remembers several key factors nd does the impartation job
 		 * for a given class.
-		 * 
+		 *
 		 * Each time the entrance method is invoked,
 		 * a new instance of this class is created,
 		 * and takes over the imartation process afterwards.
-		 * 
+		 *
 		 * @class
-		 * @param {string} usingLanguage 
+		 * @param {string} usingLanguage
 		 */
 		function WulechuanImpartationOperator(usingLanguage) {
 			var thisOperator = this;
@@ -158,6 +167,15 @@
 
 			thisOperator.startToImpart = startToImpart;
 
+
+
+
+			/**
+			 * Step 1 - to accept the function treated as a class, instances of which will be imparted.
+			 *
+			 * @param {!function} theGivenFunction
+			 * @param {?boolean} shouldNotThrowErrors
+			 */
 			function startToImpart(theGivenFunction, shouldNotThrowErrors) {
 				delete thisOperator.startToImpart;
 
@@ -174,9 +192,9 @@
 								'\n而实际提供的首个参数是一个'+typeof theGivenFunction + '。'
 								;
 							break;
-						
+
 						case languageCode_enUS:
-							errorMessage = 
+							errorMessage =
 								'The provided source must be a function, '+
 								'which will be used as a constructor '+
 								'to create the object to impart to a grantee.'+
@@ -209,13 +227,23 @@
 
 				_addAllMethodsToThisHelper();
 
-				return thisOperator; // chainable 
+				return thisOperator; // chainable
 			}
 
+			/**
+			 * Step 2 - to accept the options for construction of an instance that is to impart.
+			 *
+			 * @param {?object} constructionOptions
+			 */
 			function buildAccordingTo(constructionOptions) {
 
 			}
 
+			/**
+			 * Step 3 - to accept the name of the desired variant to use.
+			 *
+			 * @param {!string} variantName
+			 */
 			function as(variantName) {
 				if (errorOcured) return thisOperator;
 
@@ -234,10 +262,9 @@
 
 				if (errorOcured) {
 					_removeSomeMethodsOn_As_Ending();
-					return thisOperator; // chainable when no error occurs
 				}
 
-				// Here nothing returns to prevent chaining invokation.
+				return thisOperator; // must return this for chaining steps, even if errors occur.
 			}
 
 			function withCustomizedPropertyNames(propertyNamesCustomization) {
@@ -251,10 +278,9 @@
 
 				if (errorOcured) {
 					_removeSomeMethodsOn_Renaming_Ending();
-					return thisOperator; // chainable when no error occurs
 				}
 
-				// Here nothing returns to prevent chaining invokation.
+				return thisOperator; // must return this for chaining steps, even if errors occur.
 			}
 
 			function to(granteeOfMethods, granteeOfProperties) {
@@ -301,8 +327,14 @@
 							break;
 					}
 
-					return _impartIt(granteeOfMethods, granteeOfProperties);
+					// return nothing if any error occurs.
+					return;
 				}
+
+				var theInstanceThatHasBeenImparted =  _impartIt(granteeOfMethods, granteeOfProperties);
+
+				// return the instance for the grantee to store a variable within its scope.
+				return theInstanceThatHasBeenImparted;
 			}
 
 
@@ -428,18 +460,71 @@
 			function _impartIt(granteeOfMethods, granteeOfProperties) {
 				var intanceObjectToImpart = new theConstructor;
 
-				_impartMethodsTo(intanceObjectToImpart, granteeOfMethods);
-				_impartPropertiesTo(intanceObjectToImpart, granteeOfProperties);
+				for (var attributeName in intanceObjectToImpart) {
+					var attribute = intanceObjectToImpart[attributeName];
+					var attributeImpartationName = attributeName;
+
+					var shouldSkip = false;
+					if (shouldSkip) continue;
+
+					var customizedImpartationMethod = _configurations[attributeName];
+
+					if (typeof customizedImpartationMethod === 'function') {
+						customizedImpartationMethod(intanceObjectToImpart, granteeOfMethods);
+						continue;
+					}
+
+					var attributeIsAMethod = typeof attribute === 'function';
+					var attributeIsAProperty = typeof attribute !== 'function';
+
+					if (attributeIsAMethod) {
+						_impartOneMethodTheDefaultWay(
+							intanceObjectToImpart,
+							attributeName,
+							granteeOfMethods,
+							attributeImpartationName
+						);
+						continue;
+					}
+
+					if (attributeIsAProperty) {
+						_impartOnePropertyTheDefaultWay(
+							intanceObjectToImpart,
+							attributeName,
+							granteeOfProperties,
+							attributeImpartationName
+						);
+						continue;
+					}
+				}
 
 				return intanceObjectToImpart;
 			}
 
-			function _impartMethodsTo(objectToImpart, granteeOfMethods) {
-				
+			function _impartOneMethodTheDefaultWay(objectToImpart, oldName, granteeOfMethod, newName) {
+				Object.defineProperty(objectToImpart, newName,
+					{
+						enumerable: true,
+						get: function () {
+							return objectToImpart[oldName];
+						}
+						// method should have no setter
+					}
+				);
 			}
 
-			function _impartPropertiesTo(objectToImpart, granteeOfProperties) {
-				
+			function _impartOnePropertyTheDefaultWay(objectToImpart, propertyOldName, granteeOfProperty, propertyNewName) {
+				Object.defineProperty(objectToImpart, propertyNewName,
+					{
+						enumerable: true,
+						get: function () {
+							return objectToImpart[propertyOldName];
+						},
+						set: function (newValue) {
+							objectToImpart[propertyOldName] = newValue;
+						}
+					}
+				);
 			}
 		}
 	}
